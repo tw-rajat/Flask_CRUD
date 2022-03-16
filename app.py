e75ccbc5b906c40ad9b891fb07fdced4d0470e4c
Rajat Srivastava <rajatmyself@gmail.com>
2022-03-16 11:06:13 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'FLASKCRUD'
+app.config['MAX_CONTENT-PATH'] = '500000'
+db =SQLAlchemy(app)
+
+class EmployeeModel(db.Model):
+    __tablename__ = "table"
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer(),unique = True)
+    name = db.Column(db.String())
+    age = db.Column(db.Integer())
+    position = db.Column(db.String(80))
+ 
+    def __init__(self, employee_id,name,age,position):
+        self.employee_id = employee_id
+        self.name = name
+        self.age = age
+        self.position = position
+ 
+    def __repr__(self):
+        return f"{self.name}:{self.employee_id}"
+  
+@app.before_first_request
+def create_table():
+    db.create_all()
+ 
+@app.route('/data/create' , methods = ['GET','POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('createpage.html')
+ 
+    if request.method == 'POST':
+        employee_id = request.form['employee_id']
+        name = request.form['name']
+        age = request.form['age']
+        position = request.form['position']
+        employee = EmployeeModel(employee_id=employee_id, name=name, age=age, position = position)
+        db.session.add(employee)
+        db.session.commit()
+        return redirect('/data')
+ 
+@app.route('/')
+def welcome():
+    return render_template('welcome.html') 
+
+@app.route('/data')
+def RetrieveList():
+    employees = EmployeeModel.query.all()
+    return render_template('datalist.html',employees = employees)
+ 
+ 
+@app.route('/data/<int:id>')
+def RetrieveEmployee(id):
+    employee = EmployeeModel.query.filter_by(employee_id=id).first()
+    if employee:
+        return render_template('data.html', employee = employee)
+    return f"Employee with id ={id} Doenst exist"
+ 
+ 
+@app.route('/data/<int:id>/update',methods = ['GET','POST'])
+def update(id):
+    employee = EmployeeModel.query.filter_by(employee_id=id).first()
+    if request.method == 'POST':
+        if employee:
+            db.session.delete(employee)
+            db.session.commit()
+            name = request.form['name']
+            age = request.form['age']
+            position = request.form['position']
+            employee = EmployeeModel(employee_id=id, name=name, age=age, position = position)
+            db.session.add(employee)
+            db.session.commit()
+            return redirect(f'/data/{id}')
+        return f"Employee with id = {id} Does nit exist"
+ 
+    return render_template('update.html', employee = employee)
+ 
+ 
+@app.route('/data/<int:id>/delete', methods=['GET','POST'])
+def delete(id):
+    employee = EmployeeModel.query.filter_by(employee_id=id).first()
+    if request.method == 'POST':
+        if employee:
+            db.session.delete(employee)
+            db.session.commit()
+            return redirect('/data')
+        #abort(404)
+ 
+    return render_template('delete.html')
+
+@app.route('/upload')  
+def upload():  
+    return render_template("upload.html")  
+ 
+@app.route('/uploader', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)  
+        return 'File Uploaded Successfully'
+
+if __name__ == "__main__":
+    app.run(debug = True)
